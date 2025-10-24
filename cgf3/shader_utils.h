44391406ed944889fdd12b1d713cd3d96c061df6
Zanirina <beketovazarina.bz@gmail.com>
2025-10-24 08:38:30 +0500

    part

---
+++ cgf3/shader_utils.h
@@ -0,0 +1,59 @@
+#pragma once
+#include <GL/glew.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+inline std::string loadShaderSource(const std::string& filepath) {
+    std::ifstream file(filepath);
+    if (!file.is_open()) {
+        std::cerr << "❌ Error: couldn't open shader" << filepath << std::endl;
+        return "";
+    }
+    std::stringstream buffer;
+    buffer << file.rdbuf();
+    return buffer.str();
+}
+
+inline GLuint compileShader(GLenum type, const std::string& source) {
+    GLuint shader = glCreateShader(type);
+    const char* src = source.c_str();
+    glShaderSource(shader, 1, &src, nullptr);
+    glCompileShader(shader);
+
+    GLint success;
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        char info[512];
+        glGetShaderInfoLog(shader, 512, nullptr, info);
+        std::cerr << "❌ Ошибка компиляции шейдера:\n" << info << std::endl;
+    }
+    return shader;
+}
+
+inline GLuint createShaderProgram(const std::string& vertexPath, const std::string& fragmentPath) {
+    std::string vSrc = loadShaderSource(vertexPath);
+    std::string fSrc = loadShaderSource(fragmentPath);
+
+    GLuint vShader = compileShader(GL_VERTEX_SHADER, vSrc);
+    GLuint fShader = compileShader(GL_FRAGMENT_SHADER, fSrc);
+
+    GLuint program = glCreateProgram();
+    glAttachShader(program, vShader);
+    glAttachShader(program, fShader);
+    glLinkProgram(program);
+
+    GLint success;
+    glGetProgramiv(program, GL_LINK_STATUS, &success);
+    if (!success) {
+        char info[512];
+        glGetProgramInfoLog(program, 512, nullptr, info);
+        std::cerr << "❌ Ошибка линковки шейдера:\n" << info << std::endl;
+    }
+
+    glDeleteShader(vShader);
+    glDeleteShader(fShader);
+
+    return program;
+}
